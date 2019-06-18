0ee1b3597e706951b8b0bdc640080c0f6eeef7e1
Nuttikrust <47714910+Nuttikrust@users.noreply.github.com>
2019-06-18 16:58:10 +0200

    Add files via upload
    Fixing the repository hopefully

---
+++ script.js
@@ -0,0 +1,57 @@
+// <!-- Tab Script -->
+let tabActive = $(document).ready(function() {
+
+	$(".tab_content").hide();
+	$(".tab_content:first").show(); 
+
+	$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active"); // Make previous tab inactive
+		$(this).addClass("active"); //Make newly selected class active
+		$(".tab_content").hide(); 
+		var activeTab = $(this).attr("rel"); 
+		$("#"+activeTab).fadeIn(); //Fade in effect
+	});
+});
+
+// <!-- Scroll to top Button -->
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+ // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("contactBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on close, close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
